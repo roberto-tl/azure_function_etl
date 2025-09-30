5b558c9ba7864078dbd743402208522466ac6da8
roberto-tl <roberto19950102@gmail.com>
2025-09-29 22:20:03 -0300

    Update blob_storage_instance.py

--- blob_storage_instance.py
+++ blob_storage_instance.py
@@ -15,9 +15,9 @@ from datetime import datetime, timezone
 from dotenv import load_dotenv
 
 
-storage_acount_key = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
-storage_account_name = "devstoreaccount1"
-connection_string = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
+storage_acount_key = 
+storage_account_name = 
+connection_string = 
 container_raw = "csv"
 container_bronze = "bronze"
 
